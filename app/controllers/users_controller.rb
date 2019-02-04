12dd5367de6dab47fddcba95c93949d8e2af2b55
Nadiafa <nadiafa581@gmail.com>
2019-02-04 15:22:37 +0000

    Add show action to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     end 
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+  end 
+
   private
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
