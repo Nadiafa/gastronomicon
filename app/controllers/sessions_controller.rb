0496895ccd92c1819b3fbd6e4038e5d1bbc096c6
Nadiafa <nadiafa581@gmail.com>
2019-02-04 18:20:55 +0000

    Add create and destroy actions' code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
+    session.clear
+    redirect_to root_path
   end
 end
