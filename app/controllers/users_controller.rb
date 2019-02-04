be6addf071105f853e0bc2fb695bec59e7e573ae
Nadiafa <nadiafa581@gmail.com>
2019-02-04 15:14:48 +0000

    Add create action and private method user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      redirect_to root_path
+    end 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
