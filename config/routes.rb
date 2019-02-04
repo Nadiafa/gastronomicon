e44eff96a3bbb8784a27670afd4893460f217406
Nadiafa <nadiafa581@gmail.com>
2019-02-04 18:18:12 +0000

    Add login and logout routes for sessions actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,27 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  ### redirect '/' to a home/welcome page
   # root      GET    /          application#home
   root 'application#home'
   
+  ### signup / create a user
   # signup    GET    /signup    users#new
   get '/signup', to: 'users#new'
   resources :users, except: [:index, :new, :update]
   
+  ### login & logout / create & destroy a session
+  # login     GET    /login    sessions#new
+  get '/login', to: 'sessions#new'
+  #           POST   /login    sessions#create
+  post '/login', to: 'sessions#create'
+  # logout    GET    /logout   sessions#destroy
+  get '/logout', to: 'sessions#destroy'
+
+
+
+
+
+
+
 
   # signup    GET    /signup          users#new
   # users     POST   /users           users#create
@@ -17,4 +30,9 @@ Rails.application.routes.draw do
   # user      GET    /users/:id       users#show
   #           DELETE /users/:id       users#destroy
 
+
+
+# sessions_new      GET /sessions/new      sessions#new
+# sessions_create   GET /sessions/create   sessions#create
+# sessions_destroy  GET /sessions/destroy  sessions#destroy
 end
