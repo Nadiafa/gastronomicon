8e9cc0a106534f542d21162922199df702ddde33
Nadiafa <nadiafa581@gmail.com>
2019-02-04 14:41:35 +0000

    Add route signup to users#new, and exclude users#new from resources :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  resources :users, except: [:index, :update]
+  # root      GET    /          application#home
   root 'application#home'
+  
+  # signup    GET    /signup    users#new
+  get '/signup', to: 'users#new'
+  resources :users, except: [:index, :new, :update]
+  
+
+  # signup    GET    /signup          users#new
+  # users     POST   /users           users#create
+  # new_user  GET    /users/new       users#new
+  # edit_user GET    /users/:id/edit  users#edit
+  # user      GET    /users/:id       users#show
+  #           DELETE /users/:id       users#destroy
+
 end
