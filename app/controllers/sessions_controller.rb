ce7f226ecf0a5dfdc2f20393a092f17311e3d118
Nadiafa <nadiafa581@gmail.com>
2019-02-14 12:04:53 +0000

    Add code to create action for users logging in via Github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,60 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    if auth
+      oauth_uid = auth["uid"]
+      oauth_username = auth["info"]["nickname"]
+      oauth_password = SecureRandom.hex
+      if user = User.find_by(uid: oauth_uid)
+        session[:user_id] = user.id
+        # redirect_to root_path
+        flash[:message] = "You have successfully logged in"
+        redirect_to user_path(user)
+      else
+        # user = User.new(uid: oauth_uid, username: auth["info"]["nickname"], password: SecureRandom.hex)
+        user = User.new(uid: oauth_uid, username: oauth_username, password: oauth_password)
+        if user.save
+          session[:user_id] = user.id
+          flash[:message] = "You have successfully logged in"
+          redirect_to user_path(user)
+        else 
+          raise user.errors.full_messages.inspect
+        end
+      end 
     else
-      redirect_to root_path
-    end
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:message] = "You have successfully logged in"
+        redirect_to user_path(@user)
+      else
+        flash[:message] = "Something went wrong."
+        redirect_to root_path
+      end
+    end 
   end
 
   def destroy
     session.clear
     redirect_to root_path
   end
+
+  private
+  
+  def auth 
+    request.env['omniauth.auth']
+  end 
 end
+
+
+# <OmniAuth::AuthHash 
+#   info=#<OmniAuth::AuthHash::InfoHash 
+#     email=nil 
+      # image="https://avatars3.githubusercontent.com/u/30985274?v=4" 
+#     name="Nadia" 
+#     nickname="Nadiafa" 
+#     urls=#<OmniAuth::AuthHash Blog="" GitHub="https://github.com/Nadiafa">
+#   > 
+#   provider="github" 
+#   uid="30985274"
+# >
