92544161896e28595b39db66c5b71364cf6667cc
Nadiafa <nadiafa581@gmail.com>
2019-02-14 16:37:29 +0000

    Add session initiation on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+      flash[:message] = "You have successfully logged in"
       redirect_to user_path(@user)
     else
+      flash[:message] = "Something went wrong"
       redirect_to root_path
     end 
   end
