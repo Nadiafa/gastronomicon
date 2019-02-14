e0b9c5e7d38b9fe202a92291dc2a35a8cc76b432
Nadiafa <nadiafa581@gmail.com>
2019-02-14 13:35:13 +0000

    Run migration

--- db/migrate/20190214133322_create_ingredient_recipes.rb
+++ db/migrate/20190214133322_create_ingredient_recipes.rb
@@ -1,6 +1,9 @@
 class CreateIngredientRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredient_recipes do |t|
+      t. integer :recipe_id
+      t. integer :ingredient_id
+      t. integer :quantity
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_132855) do
+ActiveRecord::Schema.define(version: 2019_02_14_133322) do
+
+  create_table "ingredient_recipes", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
