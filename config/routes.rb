c5307d1844627b79d6864fb92bbc3a7e5fd60df8
Nadiafa <nadiafa581@gmail.com>
2019-02-14 13:31:38 +0000

    Generate resource ingredients, and Run migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ingredients
   ### get request for omniauth / find or create user and initiate session
   get '/auth/:provider/callback', to: 'sessions#create'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_112301) do
+ActiveRecord::Schema.define(version: 2019_02_14_132855) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
