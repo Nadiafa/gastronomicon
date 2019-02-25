1dbd0ea0648fc915f551eceaabe2303722873373
Nadiafa <nadiafa581@gmail.com>
2019-02-25 18:25:30 +0000

    Change has_many :ingredients to singular, Add accepts_nested_attributes_for :ingredient, edAdd a custom setter method for ingredient_attributes to create new ingredients as long as form's ingredient fields are not empty

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,21 @@ class Recipe < ApplicationRecord
   
   belongs_to :user
   has_many :recipe_ingredients
-  has_many :ingredients, through: :recipe_ingredients
+  has_many :ingredient, through: :recipe_ingredients
+
+  accepts_nested_attributes_for :ingredient
+
+    # override ingredient setter method
+  def ingredient_attributes=(ingredient_attributes)
+    ingredient_attributes.each do |k, v|
+      
+      if !v[:name].empty?
+        if new_k = Ingredient.find_by(name: v[:name])
+          self.ingredient << new_ing
+        else
+          self.ingredient.build(name: v[:name])
+        end 
+      end
+    end 
+  end
 end
