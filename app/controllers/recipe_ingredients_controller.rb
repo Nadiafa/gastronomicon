3e6b3774cfef2e1cfd7431479b9a4e832154dc17
Nadiafa <nadiafa581@gmail.com>
2019-03-01 02:18:56 +0000

    Add new form for RecipeIngredients, and new and create actions in the controller - WITH CREATE ACTION PENDING COMPLETION

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,3 +1,24 @@
 class RecipeIngredientsController < ApplicationController
+  
+  def new
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @recipe_ingredient = RecipeIngredient.new(recipe_id: params[:recipe_id])
+  end
 
+  def create
+    @new_ingredient_name = params[:recipe_ingredient][:ingredient_attributes][:name]  #OK => returns name
+    @ingredient = Ingredient.find_or_create_by(name: @new_ingredient_name)  #OK => returns Ingredient object 
+    @recipe_ingredient = RecipeIngredient.new(recipe_ingredients_params)  # can't get quantity for params
+    if @recipe_ingredient.save
+      redirect_to user_recipe_path(current_user, @recipe)
+    else
+      render :new 
+    end 
+  end
+
+  private
+
+  def recipe_ingredients_params
+    params.require(:recipe_ingredient).permit(:recipe_id, ingredient_id: @ingredient.id, ingredient_attributes: [:quantity])
+  end
 end 
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,4 +3,6 @@ class RecipeIngredient < ApplicationRecord
 
   belongs_to :recipe
   belongs_to :ingredient
+
+  accepts_nested_attributes_for :ingredient
 end
